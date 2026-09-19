63a5dc477f96289761a3f87f3f30233074caba2f
haiqaakramkhan-arch <haiqaakramkhan@gmail.com>
2026-09-19 10:20:52 +0500

    ..
    Co-Authored-By: ghaniafahad16-rgb <255351058+ghaniafahad16-rgb@users.noreply.github.com>
    Co-Authored-By: Muhammad Ibrahim <142511645+imhibrahim@users.noreply.github.com>
    Co-Authored-By: syedah horiya <255046364+horiya693-spec@users.noreply.github.com>

--- vaccination/app/Http/Controllers/userController.php
+++ vaccination/app/Http/Controllers/userController.php
@@ -6,14 +6,14 @@ namespace App\Http\Controllers;
 class userController extends Controller
 {
     function user(){
-        return view('user.home');
+        return view('user.about');
     }
     //vaccines//
     function vaccines(){
         return view('user.vaccines');
     }
     function index(){
-        return view('user/welcome');
+        return view('user/home');
     }
 
 }
